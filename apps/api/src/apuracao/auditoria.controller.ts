098b5d8da9e53e47f50e89a77a6e80e38c33f2aa
Jaime Rosalino Vendrame Filho <jaime.vendrame@gmail.com>
2026-08-26 16:44:06 -0300

    feat: logs de auditoria e exportacao CSV (#50)

---
+++ apps/api/src/apuracao/auditoria.controller.ts
@@ -0,0 +1,46 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Query, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { AuditoriaService } from './auditoria.service';
+
+@Controller('admin/auditoria')
+export class AuditoriaController {
+  constructor(private readonly auditoriaService: AuditoriaService) {}
+
+  @Get()
+  async listar(
+    @Query('ator') ator?: string,
+    @Query('acao') acao?: string,
+    @Query('de') de?: string,
+    @Query('ate') ate?: string,
+  ) {
+    return this.auditoriaService.listar({
+      ator,
+      acao,
+      de: de ? new Date(de) : undefined,
+      ate: ate ? new Date(ate) : undefined,
+    });
+  }
+
+  @Get('csv')
+  async exportarCsv(
+    @Query('ator') ator: string | undefined,
+    @Query('acao') acao: string | undefined,
+    @Query('de') de: string | undefined,
+    @Query('ate') ate: string | undefined,
+    @Res() res: Response,
+  ): Promise<void> {
+    const csv = await this.auditoriaService.exportarCsv({
+      ator,
+      acao,
+      de: de ? new Date(de) : undefined,
+      ate: ate ? new Date(ate) : undefined,
+    });
+
+    res.set({
+      'Content-Type': 'text/csv; charset=utf-8',
+      'Content-Disposition': 'attachment; filename=auditoria.csv',
+    });
+    res.send(csv);
+  }
+}
