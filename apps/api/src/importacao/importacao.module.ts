0e2eabbdc61d73dfcf50b1d6524c83f5a14db3f6
Jaime Vendrame <jaime.vendrame@gmail.com>
2026-08-26 22:57:35 -0300

    feat: merge incremental na reimportacao (upsert por CPF)

--- apps/api/src/importacao/importacao.module.ts
+++ apps/api/src/importacao/importacao.module.ts
@@ -1,10 +1,14 @@
-﻿import { Module } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
 import { ImportacaoController } from './importacao.controller';
 import { ImportacaoService } from './importacao.service';
+import { GravacaoService } from './gravacao.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ImportacaoController],
-  providers: [ImportacaoService],
-  exports: [ImportacaoService],
+  providers: [ImportacaoService, GravacaoService],
+  exports: [ImportacaoService, GravacaoService],
 })
 export class ImportacaoModule {}
